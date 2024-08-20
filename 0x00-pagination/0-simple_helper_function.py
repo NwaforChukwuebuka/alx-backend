fc0ba265f8496d61e38b42b5eb79014ab42b01e8
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-08-20 04:00:02 +0000

    fix: fixed type annotation for tuple of size 2

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
+"""
+Defines a function named `index_range`
+"""
 from typing import Tuple
-"""
-simple helper function
-"""
 
 
-def index_range(page: int, page_size: int) -> Tuple[int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    return a tuple of size two containing a start index and an end index
+    Calculates start index and an end index corresponding to the range of
+    indexes to return in a list for those particular pagination parameters.
+    Args:
+        page (int): the current page
+        page_size (int): the amount of items in a page
+    Returns:
+        (tuple): a tuple of the start and end index for the given page
     """
-    return ((page - 1) * page_size, page * page_size)
+    nextPageStartIndex = page * page_size
+    return nextPageStartIndex - page_size, nextPageStartIndex
